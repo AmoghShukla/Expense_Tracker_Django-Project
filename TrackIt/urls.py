a39ad9cd4fb5c3d654dfb42885308e3ff558f8fb
Amogh Shukla <110371842+AmoghShukla@users.noreply.github.com>
2025-03-05 23:22:10 +0530

    Update urls.py

--- TrackIt/urls.py
+++ TrackIt/urls.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Expense/', include('Expense.urls')),  # Ensure 'Expense' app's URLs are included
-    path('accounts/', include('django.contrib.auth.urls')),  # ✅ Add this line
+    path('accounts/', include('django.contrib.auth.urls')), 
 
     # Redirect root URL to Expense List
     path('', lambda request: redirect('expense_list', permanent=True)),
